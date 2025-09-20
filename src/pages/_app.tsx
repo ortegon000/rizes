a5ee0aa68c6fadea896a8da5dfe3c479c0121718
Eduardo Ortega <ortegon000@hotmail.com>
2025-09-20 16:23:31 -0600

    feat: add HeroKeyLogo component and integrate GSAP animations
    - Created a new HeroKeyLogo component in the SVG assets.
    - Updated the global styles to include new background and logo mask styles.
    - Integrated GSAP and ScrollTrigger for animations in the Home component.
    - Modified the _app.tsx to register GSAP plugins.
    - Changed the document language from English to Spanish.
    - Refactored the index page to utilize the new HeroKeyLogo and implement animations.
    - Added Zustand store for application state management.
    - Updated TypeScript configuration to include new path aliases for assets and styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+  }, [])
+
   return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,100 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { HeroKeyLogo } from "@svg/heroKeyLogo";
+import HeroKeyBackground from "@images/hero-key-background.webp";
 
 export default function Home() {
+  const logoMaskRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const logoMask = logoMaskRef.current;
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      paused: true,
+      ease: "power2.Out",
+    });
+
+    tl
+      .to(
+        "#hero-key",
+        {
+          scale: 1,
+        },
+        0,
+      )
+      .to(
+        "#hero-key-logo",
+        {
+          opacity: 0,
+          scale: 0.5,
+        },
+        "<",
+      )
+      .to(
+        "#logo-mask",
+        {
+          ease: "power1.inOut",
+          maskSize: "clamp(150px, 25vw, 700px)",
+        },
+        "<",
+      )
+      .to(
+        "#hero-key",
+        {
+          opacity: 0,
+          duration: 0.3,
+        },
+        ">-0.3",
+      )
+      .to(
+        "#logo-mask",
+        {
+          opacity: 0,
+        },
+        ">",
+      );
+
+    ScrollTrigger.create({
+      trigger: logoMask,
+      start: "top top",
+      end: "bottom top",
+      animation: tl,
+      scrub: 1,
+      markers: true,
+    });
+
+    // Cleanup
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div ref={logoMaskRef} id="logo-mask" className="fixed z-10 top-0 w-full h-screen bg-white">
+        <section>
+          <picture
+            id="hero-key"
+            className="h-screen scale-125 w-full block overflow-hidden fixed"
           >
+            <HeroKeyLogo id="hero-key-logo" className="absolute inset-0 m-auto w-80 h-auto object-cover text-white" />
+
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              alt=""
+              id="hero-key-background"
+              src={HeroKeyBackground.src}
+              width={1920}
+              height={1080}
+              className="w-full h-full object-cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          </picture>
+        </section>
+      </div>
+    </>
   );
 }
