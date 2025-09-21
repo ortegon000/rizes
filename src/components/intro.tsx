b1faa4aeb53bce0a27976ed4a502876d79d268c2
Eduardo Ortega <ortegon000@hotmail.com>
2025-09-20 18:26:22 -0600

    feat: implement Hero and Intro components with GSAP animations

---
+++ src/components/intro.tsx
@@ -0,0 +1,78 @@
+import { HeroKeyLogo } from "@svg/heroKeyLogo";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+export default function Intro() {
+
+    const TextContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const textContainer = TextContainerRef.current;
+
+        gsap.registerPlugin(ScrollTrigger);
+
+        const tl = gsap.timeline({
+            paused: true,
+            ease: "power2.Out",
+        });
+
+        tl.to(
+            "#hero-key-text-container",
+            {
+                duration: 1,
+                ease: "power1.in",
+                maskImage:
+                    "radial-gradient(circle at 50% 0vh, black 30%, transparent 100%)",
+            },
+            0,
+        );
+        // .fromTo(
+        //     ".exit",
+        //     {
+        //         maskImage:
+        //             "radial-gradient(circle at 50% 50%, transparent 50%, black 100%)",
+        //         maskPosition: "center center",
+        //     },
+        //     {
+        //         maskImage:
+        //             "radial-gradient(circle at 50% -100%, transparent 0%, black 0%)",
+        //         maskPosition: "center center",
+        //     },
+        // );
+
+
+        ScrollTrigger.create({
+            trigger: textContainer,
+            start: "top top",
+            end: "bottom top",
+            animation: tl,
+            scrub: 1,
+            markers: true,
+        });
+
+        // Cleanup
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, []);
+
+    return (
+        <div
+            ref={TextContainerRef}
+            id="hero-key-text-container"
+            className="fixed inset-0 w-full h-full flex items-center justify-center"
+        >
+            <div>
+                <HeroKeyLogo className="absolute w-[200px] mx-auto inset-0 top-[122px] text-white" />
+                <p
+                    id="hero-key-text"
+                    className="text-7xl font-bold m-auto h-auto bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent text-center"
+                >
+                    BIENVENIDOS A LA <br /> EXPERIENCIA RIZES
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,100 +1,12 @@
-import Image from "next/image";
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { HeroKeyLogo } from "@svg/heroKeyLogo";
-import HeroKeyBackground from "@images/hero-key-background.webp";
+import Hero from "@components/hero";
+import Intro from "@components/intro";
 
 export default function Home() {
-  const logoMaskRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const logoMask = logoMaskRef.current;
-
-    gsap.registerPlugin(ScrollTrigger);
-
-    const tl = gsap.timeline({
-      paused: true,
-      ease: "power2.Out",
-    });
-
-    tl
-      .to(
-        "#hero-key",
-        {
-          scale: 1,
-        },
-        0,
-      )
-      .to(
-        "#hero-key-logo",
-        {
-          opacity: 0,
-          scale: 0.5,
-        },
-        "<",
-      )
-      .to(
-        "#hero-key-logo-mask",
-        {
-          ease: "power1.inOut",
-          maskSize: "clamp(150px, 25vw, 700px)",
-        },
-        "<",
-      )
-      .to(
-        "#hero-key",
-        {
-          opacity: 0,
-          duration: 0.3,
-        },
-        ">-0.2",
-      )
-      .to(
-        "#hero-key-logo-mask",
-        {
-          opacity: 0,
-        },
-        ">",
-      );
-
-    ScrollTrigger.create({
-      trigger: logoMask,
-      start: "top top",
-      end: "bottom top",
-      animation: tl,
-      scrub: 1,
-      markers: true,
-    });
-
-    // Cleanup
-    return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-  }, []);
-
 
   return (
     <>
-      <section ref={logoMaskRef} id="hero-key-logo-mask" className="fixed z-10 top-0 w-full h-screen bg-white">
-
-        <picture
-          id="hero-key"
-          className="h-screen scale-125 w-full block overflow-hidden fixed"
-        >
-          <HeroKeyLogo id="hero-key-logo" className="absolute inset-0 m-auto w-80 h-auto object-cover text-white" />
-
-          <Image
-            alt=""
-            id="hero-key-background"
-            src={HeroKeyBackground.src}
-            width={1920}
-            height={1080}
-            className="w-full h-full object-cover"
-          />
-        </picture>
-
-      </section>
+      <Hero />
+      <Intro />
     </>
   );
 }
