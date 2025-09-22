3971700be7dad181096512e4e8eefd490e4a6f98
Eduardo Ortega <ortegon000@hotmail.com>
2025-09-21 19:43:22 -0600

    feat: mejorar animaciones en el componente Intro y ajustar estilos de entrada y salida

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -16,48 +16,49 @@ export default function Intro() {
 
         const tl = gsap.timeline({
             paused: true,
-            ease: "power2.Out",
+            ease: "power2.inOut",
         });
 
-        // tl.to(
-        //     ".entrance",
-        //     {
-        //         ease: "power1.in",
-        //         maskImage:
-        //             "radial-gradient(circle at 50% 0vh, black 30%, transparent 100%)",
-        //     },
-        //     0
-        // )
-        //     .to("#hero-text-intro-logo", {
-        //         duration: 1,
-        //         opacity: 1,
-        //     }, "<-1")
-        //     .fromTo(
-        //         ".exit",
-        //         {
-        //             maskImage:
-        //                 "radial-gradient(circle at 50% 50%, transparent 50%, black 100%)",
-        //             maskPosition: "center center",
-        //         },
-        //         {
-        //             duration: 2,
-        //             ease: "power1.in",
-        //             maskImage:
-        //                 "radial-gradient(circle at 50% -100%, transparent 0%, black 0%)",
-        //             maskPosition: "center center",
-        //         },
-        //         '>1'
-        //     );
+        tl.fromTo(
+            ".entrance",
+            {
+                maskImage:
+                    "radial-gradient(circle at 50% 10%, black 50%, transparent 100%)",
+            },
+            {
+                duration: 1.5,
+                maskImage:
+                    "radial-gradient(circle at 50% -100%, black 50%, transparent 50%)",
+            },
+            2.4
+        )
+            .to("#hero-text-intro-logo", {
+                duration: 1,
+                opacity: 1,
+            }, "<-1")
+            .fromTo(
+                ".exit",
+                {
+                    maskImage:
+                        "radial-gradient(circle at 50% 50%, transparent 50%, black 100%)",
+                },
+                {
+                    duration: 1.5,
+                    maskImage:
+                        "radial-gradient(circle at 50% -100%, transparent 50%, black 50%)",
+                },
+                '<3'
+            );
 
 
-        // ScrollTrigger.create({
-        //     trigger: textContainer,
-        //     start: "top top",
-        //     end: "bottom top",
-        //     animation: tl,
-        //     scrub: 1,
-        //     markers: true,
-        // });
+        ScrollTrigger.create({
+            trigger: textContainer,
+            start: "top top",
+            end: "bottom top",
+            animation: tl,
+            scrub: 1,
+            markers: true,
+        });
 
         // Cleanup
         return () => {
@@ -67,15 +68,15 @@ export default function Intro() {
 
     return (
         <>
-            <section>
-                <div className="entrance"></div>
+            <section id="hero-text-intro" className="fixed inset-0 w-full h-100dvh">
+                <div className="entrance w-full h-full bg-[#1d1b22] absolute z-50 top-0 left-0"></div>
 
                 <div
                     ref={TextContainerRef}
                     id="hero-text-container"
-                    className="fixed inset-0 w-full h-full flex items-center justify-center"
+                    className="flex w-full h-full items-center justify-center"
                 >
-                    <div>
+                    <div className="">
                         <Logo id="hero-text-intro-logo" className="absolute w-[200px] mx-auto inset-0 top-[12.9%] text-white opacity-0" />
                         <p
                             id="hero-text"
@@ -86,7 +87,7 @@ export default function Intro() {
                     </div>
                 </div>
 
-                <div className="exit"></div>
+                <div className="exit w-full h-full bg-[#1d1b22] absolute z-50 top-0 left-0"></div>
             </section>
         </>
     )
