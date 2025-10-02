1db057a7b18d972dcc2b1300a792cc8977f90e32
Eduardo Ortega <ortegon000@hotmail.com>
2025-10-02 15:49:45 -0600

    fix: corregir atributos de SVG en el componente HorizontalScrollView

--- src/components/horizontalScroll.tsx
+++ src/components/horizontalScroll.tsx
@@ -194,8 +194,8 @@ const HorizontalScrollView: React.FC<HorizontalScrollViewProps> = ({
           }`}
         style={{ zIndex: 1010 }}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
         </svg>
 
         Atras
